eb49b266f694533165337d1b44fe23292a05bcef
Stuart Terrett <shterrett@gmail.com>
2020-04-05 11:29:55 -0400

    Try for Initial <=> Final

---
+++ src/SectionTwo/Iso.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module SectionTwo.Iso where
+
+import SectionTwo.Initial (Exp(..))
+import SectionTwo.Final (ExpSYM(..))
+import SectionTwo.FinalMult (MultSYM(..))
+
+iToF :: (ExpSYM a, MultSYM a) => Exp -> a
+iToF (Lit x) = lit x
+iToF (Neg e) = neg (iToF e)
+iToF (Add e1 e2) = add (iToF e1) (iToF e2)
+iToF (Mult e1 e2) = mult (iToF e1) (iToF e2)
+
+fToI :: (ExpSYM a, MultSYM a) => a -> Exp
+fToI = undefined
+
+instance ExpSYM Exp where
+    lit = Lit
+    neg = Neg
+    add e1 e2 = Add e1 e2
+
+instance MultSYM Exp where
+    mult e1 e2 = Mult e1 e2
